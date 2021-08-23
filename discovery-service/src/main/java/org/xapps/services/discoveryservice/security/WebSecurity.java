4a6d56580d2df6ae08c78fb152412c9a9e631d9f
Camilo del Real <cjdelreal2009@gmail.com>
2021-08-23 14:44:21 +0200

    ADD: Discovery service

---
+++ discovery-service/src/main/java/org/xapps/services/discoveryservice/security/WebSecurity.java
@@ -0,0 +1,21 @@
+package org.xapps.services.discoveryservice.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/status/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+}
---
+++ discovery-service/src/test/java/org/xapps/services/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xapps.services.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
