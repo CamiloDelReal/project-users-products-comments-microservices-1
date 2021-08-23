7f7f33b125c0c9e192e3aec77e692a8327e71384
Camilo del Real <cjdelreal2009@gmail.com>
2021-08-23 14:45:07 +0200

    ADD: Gateway service

---
+++ gateway-service/src/main/java/org/xapps/services/gatewayservice/controllers/ServiceController.java
@@ -0,0 +1,28 @@
+package org.xapps.services.gatewayservice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "/status")
+public class ServiceController {
+
+    private Environment env;
+
+
+    @Autowired
+    public ServiceController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping(path = "/check")
+    public String check() {
+        return "Gateway service running with\n" +
+                "\tPort: " + env.getProperty("server.port");
+    }
+
+}
